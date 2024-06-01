ee3d33de024a656728ff1c8557d9138062df95ee
Abhishek Chaurasia <107066993+Abhithecoder03@users.noreply.github.com>
2024-06-02 01:33:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const bodyParser = require("body-parser");
 const app = express();
 //connecting to DB
 const mdburl='';
-mongoose.connect(mdburl,{useNewUrlParser: true,useUnifiedTopology:true})
+mongoose.connect(mdburl)
 .then((result)=>app.listen(3000))
 .catch((err)=>console.log(err));
 
